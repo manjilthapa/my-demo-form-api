ccf30e2a26ee145d54f005ca2e6b2bed853e85ae
manjilthapa <manjilthapa@gmail.com>
2020-09-14 20:57:37 +0200

    file upload test form

--- contracts/create.php
+++ contracts/create.php
@@ -9,6 +9,9 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
  // include database and object files
 include_once '../config/database.php';
 include_once '../objects/contract.php';
+
+require('./vendor/autoload.php');
+
  
 // get database connection
 $database = new Database();
@@ -32,8 +35,37 @@ $contract->who_pay = isset($item['who_pay']) ? $item['who_pay']: "";
 $contract->start_date = isset($item['start_date']) ? $item['start_date']: "";
 $contract->end_date = isset($item['end_date']) ? $item['end_date']: "";
 $contract->comment = isset($item['comment']) ? $item['comment']: "";
-$contract->file = isset($item['file']) ? $item['file']: "";
+$contract->file_path =  "";
 
+//Save file to aws s3
+if(isset($_FILES['file']) 
+    && $_FILES['file']['error'] == UPLOAD_ERR_OK 
+    && is_uploaded_file($_FILES['file']['tmp_name'])) {
+    
+    // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+    $s3 = new Aws\S3\S3Client([
+        'version'  => '2006-03-01',
+        'region'   => 'eu-north-1',
+    ]);
+    $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+    // FIXME: you should add more of your own validation here, e.g. using ext/fileinfo
+    $file_Path = $_FILES['file']['tmp_name'];
+    $key = basename($_FILES['file']['name']);
+    try {
+        // FIXME: you should not use 'name' for the upload, since that's the original filename from the user's computer - generate a random filename that you then store in your database, or similar
+        //$upload = $s3->upload($bucket, $_FILES['userfile']['name'], $_FILES['userfile']['tmp_name'], 'public-read');
+        $result = $s3->putObject([
+            'Bucket' => $bucket,
+            'Key' => $key,
+            'SourceFile' => $file_Path,
+            'ACL'    => 'public-read',
+            'ContentType'  => $_FILES['file']['type']
+        ]);
+        $contract->file_path =  $result['ObjectURL'];
+    } catch(Exception $e) { 
+        echo json_encode(array("upload" => "Error"));
+    } 
+}
 
 if($contract->create()){
 
