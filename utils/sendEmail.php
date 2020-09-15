83be7dbb7d9974fe0318b4b4024a9e20da8d7648
manjilthapa <manjilthapa@gmail.com>
2020-09-15 22:06:56 +0200

    added sendgrid to send email

---
+++ utils/sendEmail.php
@@ -0,0 +1,23 @@
+<?php
+// Load Composer's autoloader
+require '../vendor/autoload.php';
+
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("andtest3232@gmail.com", "Example User");
+$email->setSubject("Sending with Twilio SendGrid is Fun");
+$email->addTo("manjilthapa@gmail.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
+
+?>
