1af333cd7ed62b753fc9a26b43051f7c4442614d
manjilthapa <manjilthapa@gmail.com>
2020-09-15 11:20:04 +0200

    updating item

---
+++ contracts/update.php
@@ -0,0 +1,48 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+//header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+ // include database and object files
+include_once '../config/database.php';
+include_once '../objects/contract.php';
+
+require('../vendor/autoload.php');
+
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+// prepare user object
+$contract = new Contract($db);
+
+//$player->name = isset($_POST['name']) ? $_POST['name']: die();
+//$player->group_id = isset($_POST['group_id']) ? $_POST['group_id']: die();
+$json = file_get_contents('php://input');
+$item = json_decode($json, true);
+
+$contract->id = $item->id;
+$contract->address = $item->address;
+$contract->institution =  $item->institution;
+$contract->contact_person = $item->contact_person;
+$contract->supplier = $item->supplier;
+$contract->installation = $item->installation;
+$contract->authority_require =  $item->authority_require;
+$contract->annual_contract_amount = $item->annual_contract_amount;
+$contract->who_pay = $item->who_pay;
+$contract->start_date = $item->start_date;
+$contract->end_date =  $item->end_date;
+$contract->comment = $item->comment;
+
+
+if($contract->update()){
+    echo json_encode(array("OK" => "YES"));
+}
+else{
+    echo json_encode(array("OK" => "ERROR"));
+}
+
+?>
--- objects/contract.php
+++ objects/contract.php
@@ -107,6 +107,66 @@ class Contract{
         
     }
 
+    function update(){
+ 
+        // update query
+        $query = "UPDATE
+                    " . $this->table_name . "
+                SET
+                address=:address,institution=:institution,contact_person=:contact_person,
+                supplier=:supplier,installation=:installation,
+                authority_require=:authority_require,annual_contract_amount=:annual_contract_amount,
+                who_pay=:who_pay,start_date=:start_date, end_date=:end_date, comment=:comment
+                WHERE
+                    id = :id";
+     
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+     
+        // sanitize
+        $this->id=htmlspecialchars(strip_tags($this->id));
+        $this->address=htmlspecialchars(strip_tags($this->address));
+        $this->institution=htmlspecialchars(strip_tags($this->institution));
+        $this->contact_person=htmlspecialchars(strip_tags($this->contact_person));
+        $this->supplier=htmlspecialchars(strip_tags($this->supplier));
+        $this->installation=htmlspecialchars(strip_tags($this->installation));
+        $this->authority_require=htmlspecialchars(strip_tags($this->authority_require));
+        $this->annual_contract_amount=htmlspecialchars(strip_tags($this->annual_contract_amount));
+        $this->who_pay=htmlspecialchars(strip_tags($this->who_pay));
+        $this->start_date=htmlspecialchars(strip_tags($this->start_date));
+        $this->end_date=htmlspecialchars(strip_tags($this->end_date));
+        $this->comment=htmlspecialchars(strip_tags($this->comment));
+        $this->file_path=htmlspecialchars(strip_tags($this->file_path));
+        $this->file_name=htmlspecialchars(strip_tags($this->file_name));
+        $this->created=htmlspecialchars(strip_tags($this->created));
+    
+        // bind values
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(":address", $this->address);
+        $stmt->bindParam(":institution", $this->institution);
+        $stmt->bindParam(":contact_person", $this->contact_person);
+        $stmt->bindParam(":supplier",$this->supplier);
+        $stmt->bindParam(":installation", $this->installation);
+        $stmt->bindParam(":authority_require", $this->authority_require);
+        $stmt->bindParam(":annual_contract_amount", $this->annual_contract_amount);
+        $stmt->bindParam(":who_pay",$this->who_pay);
+        $stmt->bindParam(":start_date", $this->start_date);
+        $stmt->bindParam(":end_date", $this->end_date);
+        $stmt->bindParam(":comment", $this->comment);
+        $stmt->bindParam(":file_path",$this->file_path);
+        $stmt->bindParam(":file_name",$this->file_name);
+        $stmt->bindParam(":created", $this->created);
+        
+     
+        // execute the query
+        if($stmt->execute()){
+            
+            return true;
+        }
+     
+        return false;
+    }
+
     // delete the contract -> update status to deleted
     function delete(){
  
