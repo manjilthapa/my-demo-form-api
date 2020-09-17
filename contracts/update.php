e868c55bb7311a22666b13cb705b47904d11ff6a
manjilthapa <manjilthapa@gmail.com>
2020-09-17 12:16:07 +0200

    updating file and return new path

--- contracts/update.php
+++ contracts/update.php
@@ -36,7 +36,7 @@ $contract->who_pay = $item->who_pay;
 $contract->start_date = $item->start_date;
 $contract->end_date =  $item->end_date;
 $contract->comment = $item->comment;*/
-
+$contract->id = isset($_POST['id']) ? $_POST['id']: "";
 $contract->address = isset($_POST['address']) ? $_POST['address']: "";
 $contract->institution = isset($_POST['institution']) ? $_POST['institution']: "";
 $contract->contact_person = isset($_POST['contact_person']) ? $_POST['contact_person']: "";
@@ -51,46 +51,49 @@ $contract->comment = isset($_POST['comment']) ? $_POST['comment']: "";
 $contract->file_path = isset($_POST['file_path']) ? $_POST['file_path']: "";
 $contract->file_name = isset($_POST['file_name']) ? $_POST['file_name']: "";
 
-echo $contract->address;
 
-//Save file to aws s3
-if(isset($_FILES['file']) 
-    && $_FILES['file']['error'] == UPLOAD_ERR_OK 
-    && is_uploaded_file($_FILES['file']['tmp_name'])) {
-        echo "file is here";
-    // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-    $s3 = new Aws\S3\S3Client([
-        'version'  => '2006-03-01',
-        'region'   => 'eu-central-1',
-    ]);
-    $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-    // FIXME: you should add more of your own validation here, e.g. using ext/fileinfo
-    $file_Path = $_FILES['file']['tmp_name'];
-    $key = basename($_FILES['file']['name']);
-    
-    try {
-        // FIXME: you should not use 'name' for the upload, since that's the original filename from the user's computer - generate a random filename that you then store in your database, or similar
-        //$upload = $s3->upload($bucket, $_FILES['userfile']['name'], $_FILES['userfile']['tmp_name'], 'public-read');
-        $result = $s3->putObject([
-            'Bucket' => $bucket,
-            'Key' => $key,
-            'SourceFile' => $file_Path,
-            'ACL'    => 'public-read',
-            'ContentType'  => $_FILES['file']['type']
+if($contract->id){
+    //Save file to aws s3
+    if(isset($_FILES['file']) 
+        && $_FILES['file']['error'] == UPLOAD_ERR_OK 
+        && is_uploaded_file($_FILES['file']['tmp_name'])) {
+        // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+        $s3 = new Aws\S3\S3Client([
+            'version'  => '2006-03-01',
+            'region'   => 'eu-central-1',
         ]);
-        $contract->file_path =  $result['ObjectURL'];
-        $contract->file_name = $key;
-    } catch(Exception $e) { 
-        echo json_encode(array("upload" => $e));
-    } 
-}
+        $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+        // FIXME: you should add more of your own validation here, e.g. using ext/fileinfo
+        $file_Path = $_FILES['file']['tmp_name'];
+        $key = basename($_FILES['file']['name']);
+        
+        try {
+            // FIXME: you should not use 'name' for the upload, since that's the original filename from the user's computer - generate a random filename that you then store in your database, or similar
+            //$upload = $s3->upload($bucket, $_FILES['userfile']['name'], $_FILES['userfile']['tmp_name'], 'public-read');
+            $result = $s3->putObject([
+                'Bucket' => $bucket,
+                'Key' => $key,
+                'SourceFile' => $file_Path,
+                'ACL'    => 'public-read',
+                'ContentType'  => $_FILES['file']['type']
+            ]);
+            $contract->file_path =  $result['ObjectURL'];
+            $contract->file_name = $key;
+        } catch(Exception $e) { 
+            echo json_encode(array("upload" => $e));
+        } 
+    }
 
 
-if($contract->update()){
-    echo json_encode(array("OK" => "YES"));
-}
-else{
-    echo json_encode(array("OK" => "ERROR"));
+    if($contract->update()){
+        echo json_encode(array("OK" => "YES", "data" => array(
+            "file_path" => $contract->file_path,
+            "file_name" => $contract->file_name
+        ),));
+    }
+    else{
+        echo json_encode(array("OK" => "ERROR"));
+    }
 }
 
 ?>
