4f90e158cdf84c6836ea954270d1b70cee5754af
manjilthapa <manjilthapa@gmail.com>
2020-09-17 12:05:16 +0200

    debuging added file on update

--- contracts/update.php
+++ contracts/update.php
@@ -51,6 +51,7 @@ $contract->comment = isset($_POST['comment']) ? $_POST['comment']: "";
 $contract->file_path = isset($_POST['file_path']) ? $_POST['file_path']: "";
 $contract->file_name = isset($_POST['file_name']) ? $_POST['file_name']: "";
 
+echo $contract->address;
 //Save file to aws s3
 if(isset($_FILES['file']) 
     && $_FILES['file']['error'] == UPLOAD_ERR_OK 
